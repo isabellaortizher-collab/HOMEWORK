6570c4ca319815218733891382ad23f9eb28bf6b
Isabella Ortiz <isabella.ortiz_her@uao.edu.co>
2025-08-12 20:13:49 -0500

    correccion 2 challenge

--- challenges/01-challenge.js
+++ challenges/01-challenge.js
@@ -1,14 +1,15 @@
 // Función Regular
-function verificarParImpar(numero) {
-    console.log(numero % 2 === 0 ? `${numero} es par` : `${numero} es impar`);
+const funcionRegular = function (numero) {
+    console.log("Ejecutando la Función Regular...");
+    return numero % 2 === 0 ? `${numero} es par` : `${numero} es impar`;
 }
 
 // Función Flecha
-const verificarParImparFlecha = (numero) => {
-    console.log(numero % 2 === 0 ? `${numero} es par` : `${numero} es impar`);
+const funcionFlecha = (numero) => {
+    console.log("Ejecutando la Función Flecha...");
+    return numero % 2 === 0 ? `${numero} es par` : `${numero} es impar`;
 }
 
 // Pruebas
-verificarParImpar(4);     // 4 es par
-verificarParImparFlecha(7); // 7 es impar
-
+console.log(funcionRegular(4));  // Par
+console.log(funcionFlecha(7));   // Impar
