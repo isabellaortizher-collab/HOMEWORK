724a45cd0917567e9a0df9a150a07a028159a15a
Isabella Ortiz <isabella.ortiz_her@uao.edu.co>
2025-08-27 16:27:08 -0500

    challenge-04

---
+++ challenges/04-challenge/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+function App () {
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const handleInputChange = (e) => {
+    setCategory(e.target.value);
+
+  };
+  const handleAddCategory = () => {
+    if(category.trim()=== "") return;
+
+    setCategories([...categories, category]);
+    setCategory("");
+  }
+  return(
+    <div>
+      <h1>Challenge 04</h1>
+      <input type="text" value={category} onChange={handleInputChange} placeholder="Escribe una categoria" />
+      <button onClick={handleAddCategory}>Agregar</button>
+      <Child categories = {categories}/>
+    </div>
+  )
+}
+export default App;
+
+
---
+++ challenges/04-challenge/src/Child.js
@@ -0,0 +1,8 @@
+function Child ({categories}) {
+    return (
+        <div>
+            <h2>Lista de Categorias</h2>
+            
+        </div>
+    )
+}
